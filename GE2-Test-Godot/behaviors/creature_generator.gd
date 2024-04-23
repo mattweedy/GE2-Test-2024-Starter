fc36a9d25297e0747580724e17c8ab45d9091114
myusername <myemail>
2024-04-23 11:05:39 +0100

    added head_scene and body_scene

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -2,25 +2,25 @@
 extends Node3D
 
 @export var length:int = 10
-@export var frequency:int = 1
+@export var frequency:float = 1
 @export var start_angle:float = 0.0
 @export var base_size:float = 1.0
 @export var multiplier:float = 1.0
 
-#var head_scene = preload("res://addons")
-#var body_scene = preload("res://addons")
+var head_scene = preload("res://head_scene.tscn")
+var body_scene = preload("res://body_scene.tscn")
 
 func _process(delta):
 	if Engine.is_editor_hint():
 		pass
 
-# func draw_gizmo():
-#     var angle = start_angle
-#     for i in range(length):
-#         var size = base_size + sin(angle) * multiplier
-#         var position = Vector3(i, 0, 0)
-#         draw_cube()
-#         angle += 2 * PI * frequency / length
+func draw_gizmo():
+	var angle = start_angle
+	for i in range(length):
+		var size = base_size + sin(angle) * multiplier
+		var position = Vector3(i, 0, 0)
+		draw_cube(position, Vector3(size, size, size))
+		angle += 2 * PI * frequency / length
 
 func draw_cube(position, size):
 	var cube = MeshInstance3D.new()
